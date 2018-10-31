1dfa58a7b8e98225ce28c3732e7ff47f4176bfa9
Dhanmoni Nath <dhanmoninath989@gmail.com>
2018-10-31 21:17:11 +0530

    3rd commit

--- server.js
+++ server.js
@@ -28,10 +28,14 @@ app.get('/', (req, res)=> {
     res.json({msg:'It works'})
 })
 
+app.get('/hey', (req, res)=> {
+    res.json({msg:'It works hopefully'})
+})
+
 //use routes
 app.use('/auth', auth)
 
-const port = process.env.PORT || 80
+const port = process.env.PORT || 3001
 
 app.listen(port, ()=> {
     console.log(`Server started on port ${port}`)
